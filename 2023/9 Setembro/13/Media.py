99fe8e26c9028a4c545f7add8b20ea90a11d4168
Diego <rm552603@fiap.com.br>
2024-03-17 18:13:35 -0300

    Media e Permissao para dirigir V1

---
+++ 2023/9 Setembro/13/Media.py
@@ -0,0 +1,5 @@
+print('Programa que calcula media')
+n1 = float(input('Entre com um numero: '))
+n2 = float(input('Entre com outro numero numero: '))
+media = (n1 + n2) / 2
+print('A média é', media)
---
+++ 2023/9 Setembro/13/Permissao para dirigir V1.py
@@ -0,0 +1,9 @@
+print("Permissao para dirigir")
+#versao 1: somente o desvio condicional simples
+idade = int(input("Qual sua idade? "))
+if idade >= 18:
+  print("Voce pode dirigir")
+  print("Mas tem que ter habilitacao")
+print("Obrigada por participar")
+
+print("Permissao para dirigir")
