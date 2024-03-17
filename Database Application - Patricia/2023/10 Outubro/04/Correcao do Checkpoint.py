cb77d1c032e6357ec31f9df048a7bbbfdd7d802a
Diego <rm552603@fiap.com.br>
2024-03-17 20:42:50 -0300

    Correcao do Checkpoint

---
+++ Database Application - Patricia/2023/10 Outubro/04/Correcao do Checkpoint.py
@@ -0,0 +1,13 @@
+# Correcao do Checkpoint
+
+print("Acordo da Damp")
+dia = int(input("Qual o dia da semana (1- domingo, 7-sabado)? "))
+if dia == 6:
+  licao = input("Voce fez a licao de casa? ").lower().strip()
+  banho = input("Voce deu banho no SNIFF? ").lower().strip()
+  if licao in ('sim', 's', 'yes', 'y') and banho in ('sim', 's', 'yes', 'y'):
+    print("Oba vamos tomar sorvete na DAMP")
+  else:
+    print("Voce deve fazer a licao e lavar o cachorro")
+else:
+  print("So vamos tomar sorvete na sexta")
