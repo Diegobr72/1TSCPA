cad874ea0e12d10181d74fcf379239405c20b641
Diego <rm552603@fiap.com.br>
2024-03-17 19:46:32 -0300

    PIM

--- Database Application - Patricia/2023/10 Outubro/04/PIM.py
+++ Database Application - Patricia/2023/10 Outubro/04/PIM.py
@@ -1,10 +1,10 @@
-print("Jogo do PIM")
+#O jogo do PIM era uma brincadeira conhecida do Silvio Santos em seu programa de auditório que consistia em pedir a alguém que recite uma sequência numérica iniciada em 1 e caso o número seja múltiplo de quatro deveria substitui-lo pela palavra PIM. Faça um programa que escreva na tela uma sequência de 1 a 30 substituindo os múltiplos de quatro pela palavra PIM.
 
-#nao esta correto pois aqui somente vai ate o 29
-#for i in range (1, 30):
+print("Bem vindo ao jogo PIM")
+limite = int(input("Qual é o numero limite que você deseja?"))
 
-for i in range (1, 31):
-  if i % 4 == 0:
-      print("PIM")
-  else:
-      print(i)
+for numero in range(limite + 1):
+    if numero % 4 == 0:
+        print("PIM")
+    else:
+        print(numero)
