0db9ab3d88df5052ea18a22a4395c24ee17f9456
Diego <rm552603@fiap.com.br>
2024-03-17 18:40:14 -0300

    Decisao por selecao

---
+++ 2023/9 Setembro/20/Decisao por selecao.py
@@ -0,0 +1,22 @@
+print("Dias da semana")
+
+dia = int(input("Informe o dia: "))
+if dia == 6:
+  print("SEXTA")
+elif dia == 2:
+  print("segunda")
+elif dia == 3:
+  print("terca")
+elif dia == 4:
+  print("quarta")
+elif dia == 5:
+  print("quinta")
+elif dia == 7:
+  print("sabado")
+elif dia == 1:
+  print("domingo")
+else:
+  print("dia invalido")
+
+# #Decisao sequencial é ineficiente
+# #NAO USAR
