b1f60a13adbfee0b610b14fae56ab7d66a533178
Diego <rm552603@fiap.com.br>
2024-03-17 18:26:46 -0300

    Programa da Caixa DAgua V2

---
+++ 2023/9 Setembro/13/Programa da Caixa DAgua V2.py
@@ -0,0 +1,8 @@
+#versao 2: Programa da Caixa DAgua
+
+print("Programa da Caixa DAgua - versao 2")
+bloco = int(input('Qual bloco vc mora? '))
+if bloco % 2 == 0:
+  print("A sua caixa d'agua é a A")
+else:
+  print("A sua caixa d'agua é a B")
