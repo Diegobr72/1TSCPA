c6b38b95dd6132d7e89f63d211c6c3e203eaab80
Diego <rm552603@fiap.com.br>
2024-03-17 19:37:46 -0300

    Tabuada V2.py

---
+++ Database Application - Patricia/2023/10 Outubro/04/Tabuada/Tabuada V2.py
@@ -0,0 +1,5 @@
+print("Tabuada com o FOR")
+numero = int(input("Qual tabuada deseja calcular? "))
+print("Com a colecao especificada")
+for i in (1,2,3,4,5,6,7,8,9,10,11,12,13):
+  print(numero, "X", i, "=", numero * i)
