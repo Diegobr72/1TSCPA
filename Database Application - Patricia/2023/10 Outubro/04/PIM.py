414586ef961b3d81dd1f12bc8a8a9fa8dca324c8
Diego <rm552603@fiap.com.br>
2024-03-17 19:42:33 -0300

    PIM

---
+++ Database Application - Patricia/2023/10 Outubro/04/PIM.py
@@ -0,0 +1,10 @@
+print("Jogo do PIM")
+
+#nao esta correto pois aqui somente vai ate o 29
+#for i in range (1, 30):
+
+for i in range (1, 31):
+  if i % 4 == 0:
+      print("PIM")
+  else:
+      print(i)
