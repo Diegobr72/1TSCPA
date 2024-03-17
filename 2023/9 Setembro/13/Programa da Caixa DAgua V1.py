e07e532fcc3b729aab39b85152304de5290d2d56
Diego <rm552603@fiap.com.br>
2024-03-17 18:25:36 -0300

    Permissao para dirigir V4

---
+++ 2023/9 Setembro/13/Programa da Caixa DAgua V1.py
@@ -0,0 +1,8 @@
+#versao 1: Programa da Caixa DAgua
+
+print("Programa da Caixa DAgua")
+bloco = input('Qual bloco vc mora? ')
+if bloco == '2' or bloco == '4':
+  print("A sua caixa d'agua é a A")
+else:
+  print("A sua caixa d'agua é a B")
