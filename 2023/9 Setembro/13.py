e1f615976275e1f8ad19482fba9c85c73bd2e281
Diego <rm552603@fiap.com.br>
2024-03-17 17:44:38 -0300

    primeiro codigo

---
+++ 2023/9 Setembro/13.py
@@ -0,0 +1,9 @@
+idade = int(input("Qual sua idade? "))
+if idade >= 18:
+  print("Voce pode dirigir")
+  print("Mas tem que ter habilitacao")
+else:
+  print("Você NÃO PODE dirigir")
+print("Obrigada por participar")
+
+print("Permissao para dirigir")
