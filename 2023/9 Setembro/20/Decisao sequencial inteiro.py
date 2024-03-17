8017cfea331a82bd82c734bc0b0545f7cdd56513
Diego <rm552603@fiap.com.br>
2024-03-17 18:35:53 -0300

    Decisao sequencial inteiro

---
+++ 2023/9 Setembro/20/Decisao sequencial inteiro.py
@@ -0,0 +1,8 @@
+print("Faixa de Valores para inteiro")
+numero = int(input("Entre com um numero: "))
+if numero >=1 and numero <=10:
+  print("Primeira Faixa")
+if numero >=11 and numero <=20:
+  print("Segunda Faixa Faixa")
+if numero >=21 and numero <=30:
+  print("Terceira Faixa")
