936dce7396e23e3a7014fc4ab02200da5cf650ce
Diego <rm552603@fiap.com.br>
2024-03-17 21:25:26 -0300

    Contador e pares e impas com ia

--- Database Application - Patricia/2023/10 Outubro/11/Versao gerada pela IA com o FOR.py
+++ Database Application - Patricia/2023/10 Outubro/11/Versao gerada pela IA com o FOR.py
@@ -1,4 +1,3 @@
-#Versao gerada pela IA com o FOR
 n1 = int(input("Digite o primeiro número: "))
 n2 = int(input("Digite o segundo número: "))
 
@@ -10,3 +9,5 @@ for i in range(n1, n2 + 1):
         pares += 1
     else:
         impares += 1
+
+print(f"Entre {n1} e {n2} há {pares} números pares e {impares} números ímpares.")
