7704f9448f32ae18a91fac28a494d0a220b6047b
Diego <rm552603@fiap.com.br>
2024-03-17 21:27:31 -0300

    numeros pares.py

---
+++ Database Application - Patricia/2023/10 Outubro/11/numeros pares.py
@@ -0,0 +1,5 @@
+#(Em diagrama de blocos e depois em Python) Crie um algoritmo que imprima os numeros pares de 1 até 600
+
+for numero in range(1, 601):
+    if numero % 2 == 0:
+        print(numero)
