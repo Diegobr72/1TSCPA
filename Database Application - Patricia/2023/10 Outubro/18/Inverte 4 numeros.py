3779924e90da96911e906a545f10e7093797dd37
Diego <rm552603@fiap.com.br>
2024-03-21 23:51:00 -0300

    inverter corrigido

--- Database Application - Patricia/2023/10 Outubro/18/Inverte 4 numeros.py
+++ Database Application - Patricia/2023/10 Outubro/18/Inverte 4 numeros.py
@@ -1,5 +1,4 @@
 numero = int(input("Digite um número inteiro de 4 dígitos: "))
-numero = 9876
 if 1000 <= numero <= 9999:
 
     d1 = numero % 10 #6
@@ -17,9 +16,3 @@ if 1000 <= numero <= 9999:
     inverso = inverso * 10 + d4 # 678 * 10 + 9 = 6789
 
     numero_invertido = d1 * 1000 + d2 * 100 + d3 * 10 + d4
-
-
-    print("Número invertido:", numero_invertido)
-    print("Prof inverso", inverso)
-else:
-    print("Por favor, digite um número inteiro válido de 4 dígitos.")
