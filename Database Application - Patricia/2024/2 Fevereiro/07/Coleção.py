7cdcf3ce6c40420b4ea7de00a34ba74426637ea5
Diego <rm552603@fiap.com.br>
2024-03-22 00:03:26 -0300

    Coleção

---
+++ Database Application - Patricia/2024/2 Fevereiro/07/Coleção.py
@@ -0,0 +1,9 @@
+print('Listas\n')
+minhaLista = ['cafe', 'acucar','agua']
+print(minhaLista)
+                #0      #1      #2    #3      #4
+minhaLista = ['cafe', 'acucar','agua','cafe','cafe']
+print(minhaLista)
+
+print('Criando listas a partir de outra lista')
+                #zero:até-1
