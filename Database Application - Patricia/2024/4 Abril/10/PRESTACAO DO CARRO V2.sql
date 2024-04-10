c3e1c7d91de77dbac95183d6c29f9eab54f85623
Diego <rm552603@fiap.com.br>
2024-04-10 10:20:12 -0300

    PRESTACAO DO CARRO V2.sql

---
+++ Database Application - Patricia/2024/4 Abril/10/PRESTACAO DO CARRO V2.sql
@@ -0,0 +1,19 @@
+SET SERVEROUTPUT ON
+
+ACCEPT saldo_conta NUMBER PROMPT 'Qual o saldo da conta corrente? ';
+ACCEPT prestacao_carro NUMBER PROMPT 'Qual o valor da prestação do carro? ';
+
+DECLARE
+    novo_saldo NUMBER;
+    mes NUMBER := 1;
+BEGIN
+    novo_saldo := &saldo_conta;
+    LOOP
+        novo_saldo := novo_saldo - &prestacao_carro;
+        EXIT WHEN novo_saldo < &prestacao_carro;
+        DBMS_OUTPUT.PUT_LINE('Ao final do mês ' || mes || ' você terá ' || novo_saldo || ' reais');
+        mes := mes + 1;
+    END LOOP;
+
+    DBMS_OUTPUT.PUT_LINE('A partir do mês ' || mes || ' você não conseguirá mais pagar a prestação');
+END;
